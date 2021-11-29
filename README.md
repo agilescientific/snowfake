edec30755e02be0959f7a8890417b10752ef6470
Matt Hall <matt@agilescientific.com>
2021-11-29 14:07:55 -0400

    adding to docs etc

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # snowfake
-Fake snow flakes
+
+[![PyPI status](https://img.shields.io/pypi/status/snowfake.svg)](https://pypi.org/project/snowfake//)
+[![PyPI versions](https://img.shields.io/pypi/pyversions/snowfake.svg)](https://pypi.org/project/snowfake//)
+[![PyPI license](https://img.shields.io/pypi/l/snowfake.svg)](https://pypi.org/project/snowfake/)
+
+Make Gravner-Griffeath "snowfakes"! This code implements:
+
+> Janko Gravner, David Griffeath (2008). Modeling snow crystal growth II: A mesoscopic lattice map with plausible dynamics. _Physica D: Nonlinear Phenomena_ **237** (3), p 385-404. [DOI: 10.1016/j.physd.2007.09.008](https://doi.org/10.1016/j.physd.2007.09.008).
+
+
+## Installation
+
+You can install this package with `pip` (be careful not to type "snowflake"):
+
+    pip install snowfake
+
+#### Experimental
+
+Installing `scikit-image` allows you to use a different affine transformation, but I haven't figured out yet if it's better or not. 
+
+    pip install snowfake[skimage]
+
+
+## Example
+
+You can produce a randome snowfake with:
+
+    import snowfake
+    s = snowfake.random()
+
+This code produces the crystal in Figure 5b of the Gravner & Griffeath (2008):
+
+```python
+from snowfake import Snowfake
+
+params =  {
+    'ρ': 0.35,  # or 'rho': 0.35 if you prefer...
+    'β': 1.4,
+    'α': 0.001,
+    'θ': 0.015,
+    'κ': 0.05,
+    'μ': 0.015,
+    'γ': 0.01,
+    'σ': 0.00005,
+    'random': False,
+}
+s = Snowfake(size=801, **params)
+s.grow()
+s.plot()
+```
+
+The various physical parameter arrays are available as `s.a` (attachment flag), `s.b` (boundary mass), `s.c` (the crystal itself) and `s.d` (the vapour). The arrays exist on hexgrids; you can rectify them with, for example, `s.rectify('c')`.
+
+The parameter `σ` (note that you can also spell out `sigma` if you prefer) can be a 1D array with one sample per epoch. This will vary the vapour density `ρ` through _time_. The parameter `ρ` can be a 2D array of shape `(size, size)`; this will vary the initial vapour density through _space_.
+
+
+## Testing
+
+You can run the tests (requires `pytest` and `pytest-cov`) with 
+
+    python run_tests.py
+
+
+---
+
+&copy; 2021 Agile Scientific, openly licenced under Apache 2.0
--- snowfake/snowfake.py
+++ snowfake/snowfake.py
@@ -19,6 +19,24 @@ from scipy.signal import convolve2d
 from scipy.ndimage import affine_transform
 import matplotlib.pyplot as plt
 from functools import partial
+try:
+    from skimage import transform
+except ImportError:
+    _has_skimage = False
+else:
+    _has_skimage = True
+
+
+greek = {v: k for k, v in {
+        'ρ': 'rho',
+        'β': 'beta',
+        'α': 'alpha',
+        'θ': 'theta',
+        'κ': 'kappa',
+        'μ': 'mu',
+        'γ': 'gamma',
+        'σ': 'sigma',
+    }.items()}
 
 
 class Snowfake:
@@ -28,19 +46,22 @@ class Snowfake:
     so you can grow it in stages, eg changing some of the parameters
     at various times.
 
-    Example parameters:
-
-    # Figure 9b.
-    params =  {
-        'ρ': 0.41,
-        'β': 1.3,
-        'α': 0.08,
-        'θ': 0.025,
-        'κ': 0.003,
-        'μ': 0.07,
-        'γ': 0.00005,
-        'σ': 0.0,
+    Example to reproduce Figure 15b in Gravner & Griffeath (2008):
+    >>> from snowfake import Snowfake
+    >>> params =   {
+        'ρ': 0.35,
+        'β': 1.4,
+        'α': 0.001,
+        'θ': 0.015,
+        'κ': 0.05,
+        'μ': 0.015,
+        'γ': 0.01,
+        'σ': 0.00005,
+        'random': False,
     }
+    >>> s = Snowfake(801, **params)
+    >>> s.grow()
+    >>> s.plot()
     """
 
     NBR = np.array([[0, 1, 1],
@@ -53,6 +74,37 @@ class Snowfake:
 
     def __init__(self, size, random=False, **kwargs):
         """
+        Args
+        size (int): The size in hex pixels of the 2D space.
+        random (int or bool): Whether to randomize the noise parameter, σ.
+            If 0 or False, any value for σ will be used as a bias.
+            If True, σ will be multiplied by ±1 with p = 0.5 each.
+            If any other int, it will be randomized, but using the
+            value you provide as a seed for the RNG.
+        ρ (float or 2D array): The initial value of the density of water
+            vapour in the space. If it's a 2D array, make sure it has
+            shape `(size, size)`. Typically 0.3 to 0.8.
+        β (float): The 'difficulty' of water vapour to attach to tips
+            (i.e. when a cell has only 1 or 2 neighbours). Typically
+            1 to 3.
+        α (float): Another attachment criterion. Typically 1e-3 to 1.
+        θ (float): Another attachment criterion. Typically 1e-3 to 1e-1.
+        κ (float): The proportion of the diffusive mass at each boundary
+            site that crystallizes before attachment. Typically 1e-4 to
+            1e-1.
+        μ (float): The proportion of the boundary to melt and rejoin the
+            vapour 'pool'. Typically about 0.015.
+        γ (float): The proportion of the boundary to melt and rejoin the
+            vapour 'pool'. Typically about 1e-5.
+        σ (float): The noise or bias to add to ρ at each step. Typically
+            about 1e-5; can be positive (increasing vapour over time) or
+            negative (decreasing vapour). You can also pass a 1D array-like
+            with one sample per epoch; this will change σ at that epoch.
+
+        You can also pass these parameters as `rho`, `beta`, etc.
+
+        During initialization we set up four 2D arrays:
+
         a(t): attachment at time t
         b(t): boundary mass (quasi-liquid)
         c(t): crystal mass (ice)
@@ -66,13 +118,20 @@ class Snowfake:
         This initialization corresponds to a mesoscopic prism at the origin
         surrounded by homogeneous vapor with density ρ (which could be a matrix).
         """
-        self.__dict__.update(**kwargs)
+        # Replace any spelt-out Greek parameter names:
+        params = {greek.get(k, k): v for k, v in kwargs.items()}
+    
+        
+        self.__dict__.update(**params)
         self.a = centre(size, fill=0, centre=1)
         self.b = centre(size, fill=0, centre=0)
         self.c = centre(size, fill=0, centre=1)
         self.d = centre(size, fill=self.ρ, centre=0)
-        self._random = random
-        if self._random:
+        if random is True:
+            self._random = None  # Use no seed in the RNG if random is True.
+        else:
+            self._random = random
+        if random:
             self._rng = np.random.default_rng(self._random)
         else:
             self._rng = None
@@ -82,7 +141,15 @@ class Snowfake:
         return
 
     def __repr__(self):
-        return f"Snowfake(size={self._size}, epochs={self._epochs})"
+        string  = f"Snowfake(size={self._size}, random={self._random}, "
+        string += f"ρ={self.ρ}, β={self.β}, α={self.α}, θ={self.θ}, "
+        string += f"κ={self.κ}, μ={self.μ}, γ={self.γ}, σ={self.σ})"
+        return string
+
+    def status(self):
+        string  = f"Snowfake(size={self._size}, random={self._random}, "
+        string += f"epochs={self._epochs}, attachments={int(self.a.sum())})"
+        return string
     
     @property
     def neighbours(self):
@@ -171,15 +238,20 @@ class Snowfake:
         return
     
     def rectify_skimage(self, prop='c'):
-        """Transform to Cartesian coordinates with skimage."""
-        from skimage import transform
-        img = transform.rotate(getattr(self, prop), 45.0, resize=True, mode='edge')
+        """
+        Transform to Cartesian coordinates with skimage.
+        """
+        if not _has_skimage:
+            raise ImportError("scikit-image is required to use this function.")
+        img = transform.rotate(getattr(self, prop), -45.0, resize=True, mode='edge')
         heximg = transform.rescale(img, (1/np.sqrt(3), 1))
         h, w = np.array(heximg.shape) // 2
         return heximg[:, w-h:w+h]
     
     def rectify(self, prop='c'):
-        """Transform to Cartesian coordinates with scipy."""
+        """
+        Transform to Cartesian coordinates with scipy.
+        """
         # Translation.
         origin = np.array([[1, 0, self._size/2],
                            [0, 1, self._size/2],
@@ -228,9 +300,31 @@ class Snowfake:
         plt.show()
         return
         
-    def grow(self, max_epochs=20_000, early_stopping=True, snapshot=True):
+    def grow(self, max_epochs=20_000, early_stopping=0.5, snapshot=1000):
+        """
+        Iterate over the diffusion-freezing-attachment-melting-perturbation
+        cycle, up to max_epochs times. If the flake has previously been grown,
+        calling this method again will continue to grow it. If early_stopping
+        is False, then every epoch will run.
+        
+        If early_stopping is non-zero (or not False), the snowflake will automatically
+        stop growing soon after it is the given proportion of the width of the space.
+        The default value of 0.5 has proven to be a reasonable heuristic, allowing the
+        flake to grow to a reasonable size, without boundary effects. If you pass True
+        for this parameter, the default value of 0.5 will be used. The growth loop checks
+        for early stopping every 100 epochs.
+        
+        If snapshot is non-zero (or not False) then a plot of the current state
+        will be produced every N epochs, where N = snapshot. This is on by default
+        and produces a plot every 1000 epochs.
+        """
+        if early_stopping is True:
+            early_stopping = 0.5
+        if snapshot is True:
+            snapshot = 1000
         if isinstance(self.σ, float):
             self.σ = self.σ * np.ones(max_epochs)
+
         pbar = tqdm(range(max_epochs))
         for epoch in pbar:
 
@@ -245,9 +339,9 @@ class Snowfake:
             # Some conveniences:
             pbar.set_description(f'{int(self.a.sum()):7d} attachments')
             if (epoch > 0) and early_stopping and (epoch % 100 == 0):
-                if sum(self.c[self._size//2] > 0) > 0.5 * self._size:
+                if sum(self.c[self._size//2] > 0) > early_stopping * self._size:
                     break
-            if (epoch > 0) and snapshot and (epoch % 1000 == 0):
+            if (epoch > 0) and snapshot and (epoch % snapshot == 0):
                 self.snapshot()
             self._epochs += 1
         return
@@ -256,9 +350,13 @@ class Snowfake:
 def centre(size, fill=0, centre=1):
     """
     Make an array full of something, with something else at the centre.
+    You can pass an array as fill if you prefer, eg for the initial
+    rho parameter.
     """
     arr = np.ones((size, size)) * fill
     c = int(size // 2)
+    if centre is None:
+        centre =  arr[c, c]
     arr[c, c] = centre
     return arr
 
