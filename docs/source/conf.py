05e54017d3adb62dd236bab753942eb364136d1d
Evan Bianco <evan@agilegeoscience.com>
2022-02-03 09:38:18 -0400

    Update conf.py
    changing them in the correct spot this time.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../..'))  # make docs only this
 
 # -- Project information -----------------------------------------------------
 
-project = 'alabaster'
+project = 'snowfake'
 copyright = '2022, Agile Scientific'
 author = 'Agile Scientific'
 
@@ -57,7 +57,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
